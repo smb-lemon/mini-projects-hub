79c2d22e9d57d461f035d0946e7f32ccf82a2790
smb-lemon <smblemon9@gmail.com>
2025-03-01 10:41:24 +0600

    feat: add Menu component with items props and update into app

--- menu-items/src/App.jsx
+++ menu-items/src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Title from "./Title";
+import Menu from "./Menu";
 
 const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
   return (
   <main>
     <section className="menu">
       <Title text='Our Menu'/>
+      <Menu items={menuItems} />
     </section>
   </main>
   )
