ed3724bbf92b4f9f589b2540858d0e59838d44cb
smb-lemon <smblemon9@gmail.com>
2025-03-01 16:26:19 +0600

    feat: show render menu

--- menu-items/src/MenuItem.jsx
+++ menu-items/src/MenuItem.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const MenuItem = ({img, title, price, desc}) => {
   return (
-    <h2>MenuItem</h2>
+    <article className='menu-item'>
+        <img src={img} alt={title} className='img' />
+        <div className="item-info">
+            <header>
+                <h5>{title}</h5>
+                <span className='item-price'>${price}</span>
+            </header>
+            <p className='item-text'>{desc}</p>
+        </div>
+    </article>
   )
 }
 
