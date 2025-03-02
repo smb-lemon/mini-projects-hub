4b34da7868176d0462aaa5ce827fd7614fbc8928
smb-lemon <smblemon9@gmail.com>
2025-03-02 23:49:42 +0600

    feat: added Categories to display on the homepage

--- menu-items/src/App.jsx
+++ menu-items/src/App.jsx
@@ -2,23 +2,24 @@ import { useState } from 'react';
 import Title from './Title';
 import Menu from './Menu';
 import menu from './data';
+import Categories from './Categories';
 
 // const tempCatagories = menu.map((item) =>item.category);
 // const tempSet = new Set(tempCatagories);
 // const tempItems = ['all', ...tempSet];
 // console.log(tempItems);
 
-const allCategories = new Set(menu.map((item) =>item.category));
-
+const allCategories = ['All', ...new Set(menu.map((item) =>item.category))];
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
-  const [catagories, setCatagories] = useState(allCategories);
-  console.log(allCategories);
+  const [categories, setCategories] = useState(allCategories);
+  // console.log(allCategories);
   return (
   <main>
     <section className='menu'>
       <Title text='Our Menu'/>
+      <Categories categories={categories}/>
       <Menu items={menuItems} />
     </section>
   </main>
