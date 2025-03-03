e673a93d5386642908f816063dc78d98fe5b658f
smb-lemon <smblemon9@gmail.com>
2025-03-03 22:58:13 +0600

    feat: added button functionality with filterItem function

--- menu-items/src/App.jsx
+++ menu-items/src/App.jsx
@@ -9,11 +9,15 @@ const allCategories = ['All', ...new Set(menu.map((item) =>item.category))];
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    console.log(category);
+  }
   return (
   <main>
     <section className='menu'>
       <Title text='Our Menu'/>
-      <Categories categories={categories}/>
+      <Categories categories={categories} filterItems={filterItems}/>
       <Menu items={menuItems} />
     </section>
   </main>
